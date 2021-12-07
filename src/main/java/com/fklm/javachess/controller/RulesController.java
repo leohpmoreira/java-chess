182c838e7fabb9643e0bf34f68d5e025bb76489b
leohpmoreira <90646838+leohpmoreira@users.noreply.github.com>
2021-12-07 17:36:40 -0300

    Add rule window

--- src/main/java/com/fklm/javachess/controller/RulesController.java
+++ src/main/java/com/fklm/javachess/controller/RulesController.java
@@ -7,29 +7,19 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class StartController {
+public class RulesController {
     @FXML
-    TextField player1Name;
+    Button rulesButton;
 
     @FXML
-    TextField player2Name;
-
-    @FXML
-    Button startButton;
-
-    @FXML
-    protected void onStartButtonClick(ActionEvent event){
-        ChessApplication.player1 = player1Name.getText();
-        ChessApplication.player2 = player2Name.getText();
-        System.out.println("Player 1: " + ChessApplication.player1+ " Player 2: " + ChessApplication.player2);
+    protected void onRulesButtonClick(ActionEvent event){
         try {
             Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-            FXMLLoader loader = new FXMLLoader(ChessApplication.class.getResource("rules-view.fxml"));
+            FXMLLoader loader = new FXMLLoader(ChessApplication.class.getResource("board.fxml"));
             Scene scene = new Scene(loader.load());
             stage.setScene(scene);
             stage.show();
@@ -37,5 +27,4 @@ public class StartController {
             e.printStackTrace();
         }
     }
-
 }
